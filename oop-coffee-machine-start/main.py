db1766bc3fa2797210d68027f551d19c4cf606d4
SoumyaPrabash <94521857+SoumyaPrabash@users.noreply.github.com>
2022-04-21 16:38:40 +0400

    Add files via upload

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,22 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+while is_on:
+    order_name = input(f"What would you like to have?: {menu.get_items()}")
+    if order_name == 'off':
+        is_on = False
+    elif order_name == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order_name)
+        if coffee_maker.is_resource_sufficient(drink) and  money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
